5f69281c3aa078f579ef07889bcadc2766bfd908
Haoqi Shan Xu <104140318+al-7555056@users.noreply.github.com>
2023-02-24 09:46:06 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
-
+let Decine = 0
+input.onButtonPressed(Button.A, function () {
+    Decine = randint(1, 3)
+    if (Decine == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (Decine == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            # # # # #
+            # . # . #
+            # # # # #
+            `)
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+})
